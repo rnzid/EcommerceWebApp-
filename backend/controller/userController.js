3f6ea1144b067899694064c65d4e683687584abc
rnzid <ranjitdangol13@gmail.com>
2022-07-29 00:57:15 +0545

    database connected & user schema & route work

---
+++ backend/controller/userController.js
@@ -0,0 +1,30 @@
+const signup = (req,res,next)=>{
+    const { name,email, password,role, ...rest } = req.body
+    /* console.log("password",password);
+    console.log("Name",name); */
+     // TODO: create a new user in db;
+      const hashed = bcrypt.hashSync(password, bcrypt.genSaltSync()); 
+      //console.log("key",hashed);
+      try {
+ 
+         let user = await User.create({
+            name,  
+            email,
+            password:hashed,
+            role 
+         })
+         
+         res.send({ data: user })
+        
+     }
+     catch (err) {
+         next(err)
+     }
+}
+
+const login = (req,res,next)=>{
+
+    res.send("Login controller")
+}
+
+module.exports={signup,login}
